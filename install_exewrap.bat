313a6e0de7c1e500e9ad1ee514cd7c1d81b72492
opa <opa@41ad046d-2ef8-41eb-9ac2-fc19ede17269>
2009-06-16 17:00:47 +0000

    ・install_exewrap.bat 新規追加 ・サンプルiniの記載の統一

---
+++ install_exewrap.bat
@@ -0,0 +1,41 @@
+@echo off
+:
+if not exist "wrapexec.exe" (
+	echo %~n0: wrapexec.exe not found
+	goto :eof
+)
+if not exist "wrapexecw.exe" (
+	echo %~n0: wrapexecw.exe not found
+	goto :eof
+)
+:
+call :exec1 C "samples\bat2exe.exe"
+call :exec1 C "samples\pl2exe.exe"
+call :exec1 C "samples\rb2exe.exe"
+call :exec1 C "samples\runbatch.exe"
+call :exec1 C "samples\runperl.exe"
+call :exec1 C "samples\runruby.exe"
+:
+call :exec1 G "\usrlocal\bat\CmdPrompt.exe"
+call :exec1 G "\usrlocal\bat\emacs.exe"
+call :exec1 G "\usrlocal\bat\eval.exe"
+call :exec1 G "\usrlocal\bat\gv.exe"
+call :exec1 G "\usrlocal\bat\ped.exe"
+call :exec1 G "\usrlocal\bat\pedc.exe"
+call :exec1 C "\usrlocal\bat\irb.exe"
+call :exec1 C "\usrlocal\bat\perl.exe"
+call :exec1 C "\usrlocal\bat\ruby.exe"
+:
+goto :eof
+
+:exec1
+if /i "%~1" == "C" (
+	set SRC=wrapexec.exe
+) else if /i "%~1" == "G" (
+	set SRC=wrapexecw.exe
+) else (
+	echo %~n0: unknown action
+	goto :eof
+)
+echo %SRC% -^> %~2
+copy /y "%SRC%" "%~2"
