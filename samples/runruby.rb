b94691b178f1c5c1832f4079b46924e4af48846e
opa <opa@41ad046d-2ef8-41eb-9ac2-fc19ede17269>
2009-07-06 16:43:27 +0000

    ・Rubyのサンプルコードの行末に余分な ; がついていたのを修正

--- samples/runruby.rb
+++ samples/runruby.rb
@@ -3,8 +3,8 @@
 # Example for wrapexec. Replace your script here.
 
 def main
-	print "Hello world !! I am '#$0'\n";
-	return 0;
+	print "Hello world !! I am '#$0'\n"
+	return 0
 end
 
-exit main();
+exit main()
